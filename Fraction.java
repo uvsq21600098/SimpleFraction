590ce647530bdf47784db55b8d508182fe144eae
uvsq21600098 <pierrick.taily@ens.uvsq.fr>
2020-10-02 10:24:15 +0200

    toString & Fraction Constructor

--- Fraction.java
+++ Fraction.java
@@ -1,3 +1,18 @@
 public class Fraction {
 
+    private double n;
+    private double d;
+
+    public Fraction (double n, double d) {
+        this.n = n;
+        this.d = d;
+    }
+
+    public Fraction() {
+        this(0,0);
+    }
+
+    public String toString() {
+        return Double.valueOF(n).toString() + " " + Double.valueOf(d).toString();
+    }
 }
--- Main.java
+++ Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Fraction");
+        System.out.println("Fraction : ");
+        Fraction fraction = new Fraction(5,6);
+        System.out.println(fraction.toString());
     }
 }
